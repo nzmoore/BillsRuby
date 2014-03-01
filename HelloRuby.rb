c9a43123e814c85be9a4a20f81c7085a526506e8
Bill Moore <bill@nzmoore.com>
2014-02-28 21:51:41 -0500

    Ruby learning

---
+++ HelloRuby.rb
@@ -0,0 +1,6 @@
+# comment
+puts "HelloRuby!"
+puts 1 + 1
+3.times do
+	puts "loops"
+end
---
+++ swapvariables.rb
@@ -0,0 +1,10 @@
+a = 'a'
+b = 'b'
+puts "a is " + a
+puts "b is " + b
+c = a
+a = b
+b = c
+
+puts "a is now " + a
+puts "b is now " + b
