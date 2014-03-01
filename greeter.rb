068655d29f9b8a6519ba1f9141dd08e0c858d6fe
Bill Moore <bill@nzmoore.com>
2014-03-01 12:17:30 -0500

    Add greeter and hello_sinatra

---
+++ greeter.rb
@@ -0,0 +1,5 @@
+puts "Please enter your name: "
+name = gets.chomp
+puts "Hi #{name}" 
+puts "Did you realize that your name #{name} has #{name.length} characters"
+puts "Your name reversed is #{name.reverse.capitalize}"
